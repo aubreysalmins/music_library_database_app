0deb2b489cc44fb931cdc780d78fd53f4d6a6733
Aubrey Salmins <aubreysalmins@gmail.com>
2023-05-03 07:26:17 +0100

    Project setup

---
+++ spec/integration/application_spec.rb
@@ -0,0 +1,29 @@
+require "spec_helper"
+require "rack/test"
+require_relative '../../app'
+
+describe Application do
+  # This is so we can use rack-test helper methods.
+  include Rack::Test::Methods
+
+  # We need to declare the `app` value by instantiating the Application
+  # class so our tests work.
+  let(:app) { Application.new }
+
+  context "POST /albums" do
+    it 'returns 200 OK' do
+      response = post(
+        '/albums',
+        title:'Voyage',
+        release_year:"2022",
+        artist_id:"2"
+      )
+
+      expect(response.status).to eq(200)
+      expect(response.body).to eq('')
+
+      response = get('/albums')
+      expect(response.body).to include('Voyage')
+    end
+  end
+end
